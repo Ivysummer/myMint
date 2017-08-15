7f4e1722c8a61c93ab46a40f7acb030934702815
Ivysummer <1662746188@qq.com>
2017-08-15 20:51:52 +0800

    First Commit

---
+++ src/components/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+      <mt-button type="default">default</mt-button>
+      <mt-button type="primary">primary</mt-button>
+      <mt-button type="danger" @click="toast(1,2)">danger</mt-button>
+  </div>
+</template>
+
+<script>
+
+import { Toast } from 'mint-ui';
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods:{
+      toast:function(a,b){
+          // this.$toast('Upload Complete');
+          Toast('这是弹框信息！要全局引入哦！');
+
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
