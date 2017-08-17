34fc27a537f3198d0f04a99c30003163b919e976
unknown <1662746188@qq.com>
2017-08-17 16:23:40 +0800

    完善首页

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 引入ElementUI
+import ElementUI from 'element-ui'
 // 引入MintUI
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
 
 Vue.use(MintUI)
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,77 +1,146 @@
 <template>
 	<div class="index">
-		<div class="headSearch">
-			<mt-search :value.sync="value" cancel-text="取消" placeholder="疾病/医院/科室/医生等"></mt-search>
-		</div>
+		<div class="page-tabbar ">  
+		  <div class="page-wrap">  
+		    <div class="page-title"></div>  
+		    <div>  
+		      <mt-cell class="page-part" title="当前选中" :value="selected" />  
+		    </div>  
+		
+		  <!-- tabcontainer -->  
+		    <mt-tab-container class="page-tabbar-container" v-model="selected">  
+		    <!-- 就医 -->
+		      <mt-tab-container-item id="就医">  
+		        <!-- <mt-cell v-for="n in 10" :title="'餐厅 ' + n" />  --> 
+				<div class="headSearch">
+					<div class="locate">
+						<span>广州</span>
+					</div>
+					<mt-search :value.sync="value" cancel-text="取消" placeholder="疾病/医院/科室/医生等">
+					</mt-search>
+					
+				</div>
 
-		<div class="con">
-			<div class="swipe_bar">
-				<mt-swipe :auto="4000">
-				  <mt-swipe-item><img src="../../assets/swipe1.jpg"></mt-swipe-item>
-				  <mt-swipe-item><img src="../../assets/swipe2.jpg"></mt-swipe-item>
-				  <mt-swipe-item><img src="../../assets/swipe3.jpg"></mt-swipe-item>
-				</mt-swipe>
-			</div>
-	    	<ul class="service_nav clearfix">
-	    		<li v-for="item in serListsTypes">
-	    			<figure>
-	    				<img :src="item.image_url">
-	    				<figcaption>{{ item.title }}</figcaption>
-	    			</figure>
-	    			
-	    		</li>
-	    	</ul>
-		</div>
+				<div class="con">
+					<div class="swipe_bar">
+						<mt-swipe :auto="4000">
+						  <mt-swipe-item><img src="../../assets/swipe1.jpg"></mt-swipe-item>
+						  <mt-swipe-item><img src="../../assets/swipe2.jpg"></mt-swipe-item>
+						  <mt-swipe-item><img src="../../assets/swipe3.jpg"></mt-swipe-item>
+						</mt-swipe>
+					</div>
+			    	<ul class="service_nav clearfix">
+			    		<li v-for="item in serListsTypes">
+			    			<figure>
+			    				<img :src="item.image_url">
+			    				<figcaption>{{ item.title }}</figcaption>
+			    			</figure>
+			    			
+			    		</li>
+			    	</ul>
+				</div>
+		      </mt-tab-container-item>  
+		       <!-- 健康 -->
+		      <mt-tab-container-item id="健康">  
+		        <mt-cell v-for="n in 5" :title="'订单 ' + n" />  
+		      </mt-tab-container-item> 
+		       <!-- 互动 -->
+		      <mt-tab-container-item id="互动">  
+		        <mt-cell v-for="n in 7" :title="'发现 ' + n" />  
+		      </mt-tab-container-item>  
+		      <!-- 我的 -->
+		      <mt-tab-container-item id="我的">  
+		        <div class="page-part">  
+		         <!-- cell -->  
+		          <mt-cell v-for="n in 12" :title="'我的 ' + n" />  
+		        </div>  
+		       <!--  <router-link to="/">  
+		         
+		          <mt-button type="danger" size="large">退出</mt-button>  
+		        </router-link>   -->
+		      </mt-tab-container-item>  
+		    </mt-tab-container>  
+		  </div>
+          <!-- 尾部 -->
+		  <div class="footer fixed">
+		  	<mt-tabbar v-model="selected">
+		  	  <mt-tab-item id="就医">
+		  	    <i class="fa fa-hospital-o" aria-hidden="true"></i>
+		  	    <span>就医</span>
+		  	  </mt-tab-item>
+		  	  <mt-tab-item id="健康">
+		  	    <i class="fa fa-heart-o" aria-hidden="true"></i>
+		  	    <span>健康</span>
+		  	  </mt-tab-item>
+		  	  <mt-tab-item id="互动">
+		  	    <i class="fa fa-comments-o" aria-hidden="true"></i>
+		  	    <span>互动</span>
+		  	  </mt-tab-item>
+		  	  <mt-tab-item id="我的">
+		  	    <i class="fa fa-user" aria-hidden="true"></i>
+		  	    <span>我的</span>
+		  	  </mt-tab-item>
+		  	</mt-tabbar>
+		  </div>
+
+		</div> 
 
 
 	</div>
 </template>
 
 <script>
-
+import img1 from '../../assets/drguahao1.jpg' 
+import img2 from '../../assets/guahao.png'
+import img3 from '../../assets/zhifu.png'
+import img4 from '../../assets/houzhen.jpg'
+import img5 from '../../assets/jiankangcard.jpg'
+import img6 from '../../assets/dangan.png'
 	export default {
 		data(){
 			return{
 				value:'',
+				// 中间 服务列表
 				serListsTypes:[
 					{
 						id:1,
 						title:"当日挂号",
-						image_url:"/assets/drguahao.jpg"
+						image_url:img1
 
 					},
 					{
 						id:2,
 						title:"预约挂号",
-						image_url:"../../assets/guahao.png"
+						image_url:img2
 
 					},
 					{
 						id:3,
 						title:"医疗缴费",
-						image_url:"../../assets/zhifu.png"
+						image_url:img3
 
 					},
 					{
 						id:4,
 						title:"候诊查询",
-						image_url:"../../assets/drguahao.jpg"
+						image_url:img4
 
 					},
 					{
 						id:5,
 						title:"绑定健康卡",
-						image_url:"../../assets/drguahao.jpg"
+						image_url:img5
 
 					},
 					{
 						id:6,
 						title:"健康档案",
-						image_url:"../../assets/drguahao.jpg"
-
+						image_url:img6
 					}
 
-				]
+				],
+				selected:"就医"
+
 			}
 		},
 		methods:{
@@ -83,21 +152,47 @@
 </script>
 
 <style>
+/*头部*/
+.page-tabbar {  
+    overflow: hidden;  
+    height: 100vh;  
+  }  
+  
+  .page-wrap {  
+    overflow: auto;  
+    height: 100%;  
+    padding-bottom: 100px;  
+  } 
 .headSearch{
 	position: relative;
-	width: 100%;
-    height: 50px;
+    width: 100%;
+    height: 48px;
     margin-bottom: 10px;
+    display: inline-block;
+}
+.locate{
+	position: absolute;
+	top: 50%;
+	left: 5%;
 }
 .mint-search{
 	height: 100%;
+	margin-left: 50px;
+    margin-top: 13px;
 }
-
+.mint-searchbar{
+    border-radius: 30px;
+    background-color: #E0E0E0;
+}
+.mint-searchbar-inner{
+    border-radius: 10px;
+}
+/*中间*/
 .swipe_bar{
 	position: relative;
 	width: 100%;
 	height: 200px;
-	margin-bottom: 60px;
+	margin-bottom: 40px;
 }
 .swipe_bar img{
 	width: 400px;
@@ -116,12 +211,32 @@ li{
 	margin-bottom: 20px;
 }
 li img{
-	width: 40px;
-    height: 40px;
+	width: 60px;
+    height: 60px;
     border-radius: 50%;
+    border: 1px solid #fff;
+}
+/*尾部*/
+.footer{
+	color: #fff;
+	margin-top: 10px;
+}
+.footer i{
+	display: block;
+    margin-bottom: 15px;
+    font-size: 24px;
+
+}
+.footer span{
+
+}
+.mint-tabbar > .mint-tab-item.is-selected{
+	color: #3C3C3C;
+	text-decoration: none;
 }
 
-
-	
+.mint-tab-item-label{
+	color: #3C3C3C;
+}	
 
 </style>
