33f4d2a78aa2b898d1023368ef76ed83ee199fa1
Ivysummer <1662746188@qq.com>
2017-08-17 20:16:04 +0800

    添加就医index内容

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -15,6 +15,7 @@
 				<div class="headSearch">
 					<div class="locate">
 						<span>广州</span>
+						<i class="fa fa-angle-down" aria-hidden="true"></i>
 					</div>
 					<mt-search :value.sync="value" cancel-text="取消" placeholder="疾病/医院/科室/医生等">
 					</mt-search>
@@ -22,6 +23,7 @@
 				</div>
 
 				<div class="con">
+				    <!-- 轮播图 -->
 					<div class="swipe_bar">
 						<mt-swipe :auto="4000">
 						  <mt-swipe-item><img src="../../assets/swipe1.jpg"></mt-swipe-item>
@@ -29,7 +31,8 @@
 						  <mt-swipe-item><img src="../../assets/swipe3.jpg"></mt-swipe-item>
 						</mt-swipe>
 					</div>
-			    	<ul class="service_nav clearfix">
+					<!-- 相关服务 -->
+			    	<ul class="service_nav">
 			    		<li v-for="item in serListsTypes">
 			    			<figure>
 			    				<img :src="item.image_url">
@@ -38,6 +41,31 @@
 			    			
 			    		</li>
 			    	</ul>
+			    	<!-- 资讯 -->
+					<div class="news">
+						<p>资讯</p>
+						<div class="msg-tab">
+							<mt-navbar v-model="msgSelected">
+							  <mt-tab-item id="1">推荐</mt-tab-item>
+							  <mt-tab-item id="2">保健</mt-tab-item>
+							  <mt-tab-item id="3">饮食</mt-tab-item>
+							  <mt-tab-item id="4"><i class="fa fa-plus" aria-hidden="true"></i></mt-tab-item>
+							</mt-navbar>
+
+							<!-- tab-container -->
+							<mt-tab-container v-model="msgSelected">
+							  <mt-tab-container-item id="1">
+							    <mt-cell v-for="n in 10" :title="'content ' + n" />
+							  </mt-tab-container-item>
+							  <mt-tab-container-item id="2">
+							    <mt-cell v-for="n in 4" :title="'content ' + n" />
+							  </mt-tab-container-item>
+							  <mt-tab-container-item id="3">
+							    <mt-cell v-for="n in 6" :title="'content ' + n" />
+							  </mt-tab-container-item>
+							</mt-tab-container>
+						</div>
+					</div>
 				</div>
 		      </mt-tab-container-item>  
 		       <!-- 健康 -->
@@ -62,7 +90,7 @@
 		    </mt-tab-container>  
 		  </div>
           <!-- 尾部 -->
-		  <div class="footer fixed">
+		  <div class="footer">
 		  	<mt-tabbar v-model="selected">
 		  	  <mt-tab-item id="就医">
 		  	    <i class="fa fa-hospital-o" aria-hidden="true"></i>
@@ -139,7 +167,9 @@ import img6 from '../../assets/dangan.png'
 					}
 
 				],
-				selected:"就医"
+				selected: "就医",
+				//资讯
+				msgSelected: "1",
 
 			}
 		},
@@ -155,7 +185,7 @@ import img6 from '../../assets/dangan.png'
 /*头部*/
 .page-tabbar {  
     overflow: hidden;  
-    height: 100vh;  
+    /*height: 100vh;  */
   }  
   
   .page-wrap {  
@@ -166,19 +196,20 @@ import img6 from '../../assets/dangan.png'
 .headSearch{
 	position: relative;
     width: 100%;
-    height: 48px;
+    height: 50px;
     margin-bottom: 10px;
     display: inline-block;
+    z-index: 1;
 }
 .locate{
 	position: absolute;
+	display: inline-block;
 	top: 50%;
     left: 3%;
 }
 .mint-search{
 	height: 100%;
-	margin-left: 50px;
-    margin-top: 13px;
+	margin: 10px 0px 10px 50px;
 }
 .mint-searchbar{
     border-radius: 30px;
@@ -188,7 +219,13 @@ import img6 from '../../assets/dangan.png'
 .mint-searchbar-inner{
     border-radius: 10px;
 }
+.mint-searchbar-inner input{
+    margin-left: 5px;	
+}
 /*中间*/
+.con{
+	position: relative;
+}
 .swipe_bar{
 	position: relative;
 	width: 100%;
@@ -204,6 +241,7 @@ ul.service_nav{
 	padding: 0;
 	list-style: none;
 	width: 100%;
+	overflow: hidden;
 	
 }
 li{
@@ -217,10 +255,15 @@ li img{
     border-radius: 50%;
     border: 1px solid #fff;
 }
+.news p {
+    text-align: left;
+    margin-left: 10px;
+}
 /*尾部*/
 .footer{
 	color: #fff;
 	margin-top: 10px;
+	display: fixed;
 }
 .footer i{
 	display: block;
@@ -228,8 +271,12 @@ li img{
     font-size: 24px;
 
 }
-.footer span{
+/*.footer span{
 
+
+}*/
+.mint-tabbar{
+	position: fixed;
 }
 .mint-tabbar > .mint-tab-item.is-selected{
 	color: #3C3C3C;
